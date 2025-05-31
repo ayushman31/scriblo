ec7dc9a9b37f96a2ea0741d92b9d2ee487dec8f1
Ayushman Singh <ayushmans012@gmail.com>
2025-05-31 05:30:40 +0530

    hydration error fixed

--- apps/web/src/app/utils/GetStartedDialog.tsx
+++ apps/web/src/app/utils/GetStartedDialog.tsx
@@ -13,22 +13,21 @@ export const GetStartedDialog = ({ isOpen, setIsOpen }: { isOpen: boolean, setIs
             <DialogContent className="max-w-md h-fit border-2 border-gray-300 dark:border-white bg-white dark:bg-black">
                 <DialogHeader>
                     <DialogTitle className="text-2xl font-bold text-gray-900 dark:text-white cursor-default">Get Started!</DialogTitle>
-                    <DialogDescription>
-                        <div className="space-y-4">
-                            <Input type="text" placeholder="Enter your name" className="w-full h-10 rounded-md dark:bg-gray-100 dark:text-gray-700" />
-                            <Input type="text" placeholder="Enter the Room Code (optional)" className="w-full h-10 rounded-md dark:bg-gray-100 dark:text-gray-700 uppercase placeholder:normal-case" />
-                            <Button variant={"outline"} className="w-full" onClick={() => setIsOpen(false)}>
-                                Join Room
-                            </Button>
-                        </div>
-                        <div>
-                        <p className="text-sm text-gray-500 dark:text-gray-400 text-center cursor-default mt-4">
-                            Want to create a room? <span className="text-gray-900 dark:text-white font-bold cursor-pointer" onClick={() => setIsOpen(false)}>Create Room</span>
-                        </p>
-                        </div>
-                    </DialogDescription>
                 </DialogHeader>
+
+                <div className="space-y-4 mt-4">
+                    <Input type="text" placeholder="Enter your name" className="w-full h-10 rounded-md dark:bg-gray-100 dark:text-gray-700" />
+                    <Input type="text" placeholder="Enter the Room Code (optional)" className="w-full h-10 rounded-md dark:bg-gray-100 dark:text-gray-700 uppercase placeholder:normal-case" />
+                    <Button variant={"outline"} className="w-full" onClick={() => setIsOpen(false)}>
+                        Join Room
+                    </Button>
+                </div>
+
+                <p className="text-sm text-gray-500 dark:text-gray-400 text-center cursor-default mt-4">
+                    Want to create a room? <span className="text-gray-900 dark:text-white font-bold cursor-pointer" onClick={() => setIsOpen(false)}>Create Room</span>
+                </p>
             </DialogContent>
+
         </Dialog>
     );
 };
