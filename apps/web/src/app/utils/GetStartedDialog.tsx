212dd0f02d09d821538271843a8c033d1205d453
Ayushman Singh <ayushmans012@gmail.com>
2025-06-05 01:08:15 +0530

    made some changes in redirecting from web to game

--- apps/web/src/app/utils/GetStartedDialog.tsx
+++ apps/web/src/app/utils/GetStartedDialog.tsx
@@ -4,7 +4,19 @@ import { Input } from "@/components/ui/input";
 import { useRef } from "react";
 
 export const GetStartedDialog = ({ isOpen, setIsOpen, hasRoomCode, setHasRoomCode }: { isOpen: boolean, setIsOpen: (isOpen: boolean) => void, hasRoomCode: boolean, setHasRoomCode: (hasRoomCode: boolean) => void }) => {
-    const ref = useRef<HTMLInputElement>(null);
+    const roomRef = useRef<HTMLInputElement>(null);
+    const usernameRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (roomId?: string) => {
+        const username = usernameRef.current?.value.trim();
+        if (!username) {
+            console.error("Username is required");
+            return;
+        }
+
+        window.location.href = `http://localhost:3001/room/${roomId}?username=${encodeURIComponent(username)}`;
+
+    }
     return (
         hasRoomCode ? (<Dialog open={isOpen} onOpenChange={setIsOpen} >
             <DialogTrigger asChild>
@@ -17,43 +29,43 @@ export const GetStartedDialog = ({ isOpen, setIsOpen, hasRoomCode, setHasRoomCod
                     <DialogTitle className="text-2xl font-bold text-gray-900 dark:text-white cursor-default">Get Started!</DialogTitle>
                 </DialogHeader>
 
-                <form className="space-y-4 mt-4" onSubmit={(e) => { e.preventDefault(); setIsOpen(false); window.location.href = `http://localhost:3001/room/${ref.current?.value}`; }}>
-                    <Input type="text" placeholder="Enter your name" className="w-full h-10 rounded-md dark:bg-gray-100 dark:text-gray-700" />
-                    <Input type="text" placeholder="Enter the Room Code (optional)" className="w-full h-10 rounded-md dark:bg-gray-100 dark:text-gray-700 uppercase placeholder:normal-case" ref={ref} />
+                <form className="space-y-4 mt-4" onSubmit={(e) => { e.preventDefault(); setIsOpen(false); const roomId = roomRef.current?.value.trim(); handleSubmit(roomId); }}>
+                    <Input type="text" placeholder="Enter your name" className="w-full h-10 rounded-md dark:bg-gray-100 dark:text-gray-700" ref={usernameRef} required />
+                    <Input type="text" placeholder="Enter the Room Code (optional)" className="w-full h-10 rounded-md dark:bg-gray-100 dark:text-gray-700 uppercase placeholder:normal-case" ref={roomRef} />
                     <Button variant={"outline"} className="w-full" type="submit">
                         Join Room
                     </Button>
                 </form>
 
                 <p className="text-sm text-gray-500 dark:text-gray-400 text-center cursor-default mt-4">
-                    Want to create a room? <span className="text-gray-900 dark:text-white font-bold cursor-pointer" onClick={() => {setHasRoomCode(false); }}>Create Room</span>
+                    Want to create a room? <span className="text-gray-900 dark:text-white font-bold cursor-pointer" onClick={() => { setHasRoomCode(false); }}>Create Room</span>
                 </p>
             </DialogContent>
 
         </Dialog>
-    ) : (<Dialog open={isOpen} onOpenChange={setIsOpen} >
-        <DialogTrigger asChild>
-            <Button variant="outline" size="lg" >
-                Get Started
-            </Button>
-        </DialogTrigger>
-        <DialogContent className="max-w-md h-fit border-2 border-gray-300 dark:border-white bg-white dark:bg-black">
-            <DialogHeader>
-                <DialogTitle className="text-2xl font-bold text-gray-900 dark:text-white cursor-default">Get Started!</DialogTitle>
-            </DialogHeader>
-
-            <form className="space-y-4 mt-4" onSubmit={(e) => { e.preventDefault(); setIsOpen(false); const generatedRoomId = Math.random().toString(36).substring(2, 8).toUpperCase(); window.location.href = `http://localhost:3001/room/${generatedRoomId}`; }}>
-                <Input type="text" placeholder="Enter your name" required className="w-full h-10 rounded-md dark:bg-gray-100 dark:text-gray-700" />
-                <Button variant={"outline"} className="w-full" type="submit">
-                    Create Room
+        ) : (<Dialog open={isOpen} onOpenChange={setIsOpen} >
+            <DialogTrigger asChild>
+                <Button variant="outline" size="lg" >
+                    Get Started
                 </Button>
-            </form>
+            </DialogTrigger>
+            <DialogContent className="max-w-md h-fit border-2 border-gray-300 dark:border-white bg-white dark:bg-black">
+                <DialogHeader>
+                    <DialogTitle className="text-2xl font-bold text-gray-900 dark:text-white cursor-default">Get Started!</DialogTitle>
+                </DialogHeader>
 
-            <p className="text-sm text-gray-500 dark:text-gray-400 text-center cursor-default mt-4">
-                Want to join a room? <span className="text-gray-900 dark:text-white font-bold cursor-pointer" onClick={() => {setHasRoomCode(true); }}>Join Room</span>
-            </p>
-        </DialogContent>
+                <form className="space-y-4 mt-4" onSubmit={(e) => { e.preventDefault(); setIsOpen(false); const roomId = Math.random().toString(36).substring(2, 8).toUpperCase(); handleSubmit(roomId); }}>
+                    <Input type="text" placeholder="Enter your name" required className="w-full h-10 rounded-md dark:bg-gray-100 dark:text-gray-700" ref={usernameRef} />
+                    <Button variant={"outline"} className="w-full" type="submit">
+                        Create Room
+                    </Button>
+                </form>
 
-    </Dialog>
-    ));
+                <p className="text-sm text-gray-500 dark:text-gray-400 text-center cursor-default mt-4">
+                    Want to join a room? <span className="text-gray-900 dark:text-white font-bold cursor-pointer" onClick={() => { setHasRoomCode(true); }}>Join Room</span>
+                </p>
+            </DialogContent>
+
+        </Dialog>
+        ));
 };
